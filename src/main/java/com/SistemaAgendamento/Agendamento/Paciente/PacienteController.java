dcb71212ff6095abb55fadda7af2bb05c483f99f
Matheus Freires <matheusfreires717@gmail.com>
2025-07-09 23:20:22 -0300

    feat: Adicionando os Endpoints no controller.

--- src/main/java/com/SistemaAgendamento/Agendamento/Paciente/PacienteController.java
+++ src/main/java/com/SistemaAgendamento/Agendamento/Paciente/PacienteController.java
@@ -1,4 +1,121 @@
 package com.SistemaAgendamento.Agendamento.Paciente;
 
+import com.SistemaAgendamento.Agendamento.Agendamento.AgendamentoDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pacientes")
 public class PacienteController {
+
+PacienteService pacienteService;
+
+    public PacienteController(PacienteService pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+
+    @PostMapping("/Criar")
+    public ResponseEntity<?> criar (@RequestBody PacienteDTO pacienteDTO) {
+        pacienteService.criar(pacienteDTO);
+        return ResponseEntity.ok("Paciente " + pacienteDTO.getNome() + " foi registrado.");
+    }
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> alterar(@RequestBody PacienteDTO pacienteDTO, @PathVariable Long id) {
+        PacienteDTO pacienteDTO1 = pacienteService.listarId(id);
+        if(pacienteDTO1 != null) {
+            pacienteService.atualizar(id,pacienteDTO);
+            return ResponseEntity.ok("O paciente " + pacienteDTO.getNome() + " Foi atualizado.");
+        }
+        else  {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O paciente no id " + id + " nao existe nos registros.");
+        }
+    }
+    @GetMapping("/listar")
+    public ResponseEntity<?> listar() {
+        List<PacienteDTO> list = pacienteService.lista();
+        if(list.isEmpty()) {
+            return ResponseEntity.ok("Ainda nao existe registros.");
+        } else {
+            return ResponseEntity.ok(list);
+        }
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarid(@PathVariable Long id) {
+        PacienteDTO optionalPacienteDTO = pacienteService.listarId(id);
+        if(optionalPacienteDTO != null) {
+            return ResponseEntity.ok(optionalPacienteDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O paciente no id " + id + " nao existe nos registros.");
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<?> deletar (@PathVariable Long id) {
+        PacienteDTO optionalPacienteDTO = pacienteService.listarId(id);
+        if(optionalPacienteDTO != null) {
+            pacienteService.deletar(id);
+            return ResponseEntity.ok("O paciente " + optionalPacienteDTO.getNome() + " foi removido dos registros." );
+
+        } else  {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O paciente no id " + id + " nao existe nos registros.");
+        }
+    }
+
+    @PatchMapping("/confirmar/{id}")
+    public ResponseEntity<?> confirmar (@PathVariable Long id) {
+        PacienteDTO optionalPacienteDTO = pacienteService.listarId(id);
+        if(optionalPacienteDTO != null) {
+            AgendamentoDTO agendamentoDTO = optionalPacienteDTO.getAgendamentos();
+            pacienteService.confirmar(id);
+            return ResponseEntity.ok("O agendamento de " + agendamentoDTO.getTitulo() + " foi confirmado.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O paciente no id " + id + " nao existe nos registros.");
+        }
+    }
+
+@PatchMapping("/cancelar/{id}")
+public ResponseEntity<?> cancelar (@PathVariable Long id) {
+    PacienteDTO optionalPacienteDTO = pacienteService.listarId(id);
+    if(optionalPacienteDTO != null) {
+        AgendamentoDTO agendamentoDTO = optionalPacienteDTO.getAgendamentos();
+        pacienteService.cancelar(id);
+        return ResponseEntity.ok("O agendamento de " + agendamentoDTO.getTitulo() + " foi cancelado.");
+    } else {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("O paciente no id " + id + " nao existe nos registros.");
+    }
+}
+
+    @PatchMapping("/adicionarAgendamento/{idPaciente}/{idAgendamento}")
+    public ResponseEntity<?> adicionarAgendamento(
+            @PathVariable Long idPaciente,
+            @PathVariable Long idAgendamento) {
+
+        String resultado = pacienteService.adicionarAgendamento(idPaciente, idAgendamento);
+        if (resultado.toLowerCase().contains("foi adicionado")) {
+            return ResponseEntity.ok(resultado);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultado);
+        }
+    }
+
+    @PatchMapping("/removerAgendamento/{idPaciente}")
+    public ResponseEntity<?> removerAgendamento(@PathVariable Long idPaciente) {
+        String resultado = pacienteService.removerAgendamento(idPaciente);
+        if (resultado.toLowerCase().contains("foi removido")) {
+            return ResponseEntity.ok(resultado);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultado);
+        }
+    }
+
 }
--- src/main/java/com/SistemaAgendamento/Agendamento/Paciente/PacienteService.java
+++ src/main/java/com/SistemaAgendamento/Agendamento/Paciente/PacienteService.java
@@ -1,27 +1,25 @@
 package com.SistemaAgendamento.Agendamento.Paciente;
 
-import com.SistemaAgendamento.Agendamento.Agendamento.AgendamentoDTO;
-import com.SistemaAgendamento.Agendamento.Agendamento.AgendamentoMapper;
-import com.SistemaAgendamento.Agendamento.Agendamento.AgendamentoService;
-import com.SistemaAgendamento.Agendamento.Agendamento.Status;
+import com.SistemaAgendamento.Agendamento.Agendamento.*;
+import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
+@Service
 public class PacienteService {
     PacienteRepository pacienteRepository;
     PacienteMapper pacienteMapper;
     AgendamentoService agendamentoService;
+    AgendamentoRepository agendamentoRepository;
 
-    public PacienteService(PacienteRepository pacienteRepository, AgendamentoService agendamentoService, PacienteMapper pacienteMapper) {
+    public PacienteService(PacienteRepository pacienteRepository, AgendamentoRepository agendamentoRepository, AgendamentoService agendamentoService, PacienteMapper pacienteMapper) {
         this.pacienteRepository = pacienteRepository;
+        this.agendamentoRepository = agendamentoRepository;
         this.agendamentoService = agendamentoService;
         this.pacienteMapper = pacienteMapper;
     }
-    //confirmar agendamento
-    // cancelar agendamento
-    //mostar prox consulta
-
-
 
     public PacienteDTO criar(PacienteDTO pacienteDTO) {
         pacienteRepository.save(pacienteMapper.map(pacienteDTO));
@@ -32,33 +30,85 @@ public class PacienteService {
         List<PacienteDTO> lista = (pacienteRepository.findAll()).stream().map(pacienteMapper::map).toList();
         return lista;
     }
+
     public PacienteDTO listarId(Long id) {
         PacienteDTO pacienteDTO = pacienteMapper.map(pacienteRepository.findById(id).orElse(null));
         return pacienteDTO;
     }
+
     public PacienteDTO atualizar(Long id, PacienteDTO pacienteDTO) {
         pacienteDTO.setId(id);
         pacienteRepository.save(pacienteMapper.map(pacienteDTO));
         return pacienteDTO;
     }
 
-    public void deletar (Long id) {
+    public void deletar(Long id) {
         pacienteRepository.deleteById(id);
     }
-    public AgendamentoDTO confirmar (Long id){
+
+    public AgendamentoDTO confirmar(Long id) {
         PacienteDTO pacienteDTO = pacienteMapper.map(pacienteRepository.findById(id).orElse(null));
         AgendamentoDTO agendamentoDTO = pacienteDTO.getAgendamentos();
         agendamentoService.confimarAgendamento(agendamentoDTO);
-        agendamentoService.atualizar(agendamentoDTO.getId(),agendamentoDTO);
+        agendamentoService.atualizar(agendamentoDTO.getId(), agendamentoDTO);
         return agendamentoDTO;
     }
 
-    public AgendamentoDTO cancelar (Long id){
+    public AgendamentoDTO cancelar(Long id) {
         PacienteDTO pacienteDTO = pacienteMapper.map(pacienteRepository.findById(id).orElse(null));
         AgendamentoDTO agendamentoDTO = pacienteDTO.getAgendamentos();
         agendamentoService.cancelarAgendamento(agendamentoDTO);
-        agendamentoService.atualizar(agendamentoDTO.getId(),agendamentoDTO);
+        agendamentoService.atualizar(agendamentoDTO.getId(), agendamentoDTO);
         return agendamentoDTO;
     }
 
-}
+    public String proxAgendamento(Long id) {
+        PacienteDTO pacienteDTO = pacienteMapper.map(pacienteRepository.findById(id).orElse(null));
+        String agendamentoEmTexto = pacienteDTO.getNome() + "seu agendamento atual : " + pacienteDTO.getAgendamentos().getTitulo();
+        return agendamentoEmTexto;
+    }
+    public String removerAgendamento(Long id){
+        PacienteModel pacienteModel = pacienteRepository.findById(id).orElse(null);
+        if (pacienteModel == null) {
+            return "Paciente com id " + id + " não encontrado.";
+        }
+
+        if (pacienteModel.getAgendamentos() == null) {
+            return "Esse paciente não possui nenhum agendamento para remover.";
+        }
+        if (pacienteModel.getAgendamentos().getStatus() == Status.PENDENTE) {
+            return "Esse agendamento não está disponível pois esta Pendente";
+        }
+
+        pacienteModel.setAgendamentos(null);
+        pacienteRepository.save(pacienteModel);
+
+        // Força atualizar no banco e limpar cache JPA
+        pacienteRepository.flush();
+
+        return pacienteModel.getNome() + ", seu agendamento foi removido.";
+    }
+
+    public String adicionarAgendamento(Long idPaciente, Long idAgendamento) {
+        PacienteModel pacienteModel = pacienteRepository.findById(idPaciente).orElse(null);
+        if (pacienteModel == null) {
+            return "Paciente com id " + idPaciente + " não encontrado.";
+        }
+
+        if (pacienteModel.getAgendamentos() != null) {
+            return "Você ainda possui um agendamento. Remova-o primeiro.";
+        }
+
+        AgendamentoModel agendamentoModel = agendamentoRepository.findById(idAgendamento).orElse(null);
+        if (agendamentoModel == null) {
+            return "Agendamento com id " + idAgendamento + " não encontrado.";
+        }
+            agendamentoModel.setStatus(Status.PENDENTE);
+            pacienteModel.setAgendamentos(agendamentoModel);
+            pacienteRepository.save(pacienteModel);
+
+            return "O agendamento de " + agendamentoModel.getTitulo() + " foi adicionado.";
+        }
+
+    }
+
