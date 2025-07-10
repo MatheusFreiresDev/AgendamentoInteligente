da2d21c2128a034e183e32d72c1c4da7a09cad5d
Matheus Freires <matheusfreires717@gmail.com>
2025-07-09 23:18:25 -0300

    fix: Ajustes nos mappers com tratamento de Null.

--- src/main/java/com/SistemaAgendamento/Agendamento/Agendamento/AgendamentoMapper.java
+++ src/main/java/com/SistemaAgendamento/Agendamento/Agendamento/AgendamentoMapper.java
@@ -4,22 +4,30 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AgendamentoMapper {
+
     public AgendamentoModel map(AgendamentoDTO agendamentoDTO) {
+        if (agendamentoDTO == null) return null;
+
         AgendamentoModel agendamentoModel = new AgendamentoModel();
         agendamentoModel.setId(agendamentoDTO.getId());
         agendamentoModel.setTitulo(agendamentoDTO.getTitulo());
         agendamentoModel.setDataHoraInicio(agendamentoDTO.getDataHoraInicio());
         agendamentoModel.setStatus(agendamentoDTO.getStatus());
         agendamentoModel.setListaPacientes(agendamentoDTO.getListaPacientes());
+
         return agendamentoModel;
     }
+
     public AgendamentoDTO map(AgendamentoModel agendamentoModel) {
+        if (agendamentoModel == null) return null;
+
         AgendamentoDTO agendamentoDTO = new AgendamentoDTO();
         agendamentoDTO.setId(agendamentoModel.getId());
         agendamentoDTO.setTitulo(agendamentoModel.getTitulo());
         agendamentoDTO.setDataHoraInicio(agendamentoModel.getDataHoraInicio());
         agendamentoDTO.setStatus(agendamentoModel.getStatus());
         agendamentoDTO.setListaPacientes(agendamentoModel.getListaPacientes());
+
         return agendamentoDTO;
     }
 }
--- src/main/java/com/SistemaAgendamento/Agendamento/Paciente/PacienteMapper.java
+++ src/main/java/com/SistemaAgendamento/Agendamento/Paciente/PacienteMapper.java
@@ -1,21 +1,37 @@
 package com.SistemaAgendamento.Agendamento.Paciente;
 
+import com.SistemaAgendamento.Agendamento.Agendamento.AgendamentoMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PacienteMapper {
+    private final AgendamentoMapper agendamentoMapper;
+
+    public PacienteMapper(AgendamentoMapper agendamentoMapper) {
+        this.agendamentoMapper = agendamentoMapper;
+    }
+
     public PacienteDTO map(PacienteModel pacienteModel) {
+        if (pacienteModel == null) return null;
+
         PacienteDTO pacienteDTO = new PacienteDTO();
         pacienteDTO.setId(pacienteModel.getId());
         pacienteDTO.setNome(pacienteModel.getNome());
-        pacienteDTO.setAgendamentos(pacienteModel.getAgendamentos());
+        pacienteDTO.setAgendamentos(
+                agendamentoMapper.map(pacienteModel.getAgendamentos())  // o próprio mapper já faz null check
+        );
         return pacienteDTO;
     }
+
     public PacienteModel map(PacienteDTO pacienteDTO) {
+        if (pacienteDTO == null) return null;
+
         PacienteModel pacienteModel = new PacienteModel();
         pacienteModel.setId(pacienteDTO.getId());
         pacienteModel.setNome(pacienteDTO.getNome());
-        pacienteModel.setAgendamentos(pacienteDTO.getAgendamentos());
+        pacienteModel.setAgendamentos(
+                agendamentoMapper.map(pacienteDTO.getAgendamentos())  // idem
+        );
         return pacienteModel;
     }
 }
